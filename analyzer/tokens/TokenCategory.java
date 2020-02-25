496788c412707cb7e9f8ad75f91428e0c3f86e8a
thitcc <ttcc@ic.ufal.br>
2020-02-25 17:49:24 -0300

    token category fix

--- analyzer/tokens/TokenCategory.java
+++ analyzer/tokens/TokenCategory.java
@@ -3,7 +3,7 @@ package tokens;
 public enum TokenCategory {
     unknown, funDef, funRet, main, id, paramBeg, paramEnd, commaSep, typeInt,
     typeFloat, typeBool, typeChar, typeVoid, typeStr, beginScope, endScope,
-    constInt, constFloat, colon, constBool, constChar, constStr,
+    constInt, constFloat, constVar, colon, constBool, constChar, constStr,
     opAdd, opSub, opMult, opDiv, arrBegin, arrEnd, opPow, opMod, 
     opGreater, opLesser, opGreq, opLeq, opEquals, opNotEqual,
     opAnd, opOr, opNot, opConcat, opAttrib, EOF, procDef,
